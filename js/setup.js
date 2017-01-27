bcb8b811704a4ba9f1e63b65a26564921a66b525
eugeneat@yandex.ru <eugeneath@yandex.ru>
2017-01-27 11:58:26 +0700

    parseInt fix

--- js/setup.js
+++ js/setup.js
@@ -80,9 +80,9 @@ function colorToHex(color) {
   }
   var digits = /(.*?)rgb\((\d+), (\d+), (\d+)\)/.exec(color);
 
-  var red = parseInt(digits[2]);
-  var green = parseInt(digits[3]);
-  var blue = parseInt(digits[4]);
+  var red = parseInt(digits[2], 10);
+  var green = parseInt(digits[3], 10);
+  var blue = parseInt(digits[4], 10);
 
   var rgb = blue | (green << 8) | (red << 16);
   return digits[1] + '#' + rgb.toString(16);
